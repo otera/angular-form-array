6bbec4ef835ff662c3ce252893deca8cca309099
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-06-23 16:01:56 +0900

    複数項目化前段階

--- src/app/check-input-form/check-input-form.component.ts
+++ src/app/check-input-form/check-input-form.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormControl, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-check-input-form',
-  templateUrl: './check-input-form.component.html',
-  styleUrls: ['./check-input-form.component.scss']
+  selector: "app-check-input-form",
+  templateUrl: "./check-input-form.component.html",
+  styleUrls: ["./check-input-form.component.scss"],
 })
 export class CheckInputFormComponent implements OnInit {
+  public ideaForm: FormGroup;
+  question: string[];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  get ideas(): FormArray {
+    return this.ideaForm.get("ideas") as FormArray;
   }
 
+  ngOnInit() {
+    // 問いの取得
+    this.question = ["○×ですか？", "◆♪ですか？", "■■ですか？"];
+
+    // Formの設定
+    const items: string[] = new Array(this.question.length).fill("");
+    this.ideaForm = this.fb.group({
+      ideas: this.fb.array(items),
+    });
+  }
+
+  // 疑似Submit
+  onSubmit() {
+    console.log(this.ideaForm.controls);
+  }
+
+  // 項目の追加
+  addItem(itemNumber: number) {}
+
+  // 項目の削除
+  deleteItem(itemNumber: number, index: number) {
+    // this.ideas.
+  }
 }
